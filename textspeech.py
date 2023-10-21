6029a192f72ee4a6d551217f4ebdab54bedce9dc
soup <68081409+oops408@users.noreply.github.com>
2023-10-21 16:40:36 -0700

    Update textspeech.py

--- textspeech.py
+++ textspeech.py
@@ -1,16 +1,1 @@
-from gtts import gTTS 
 
-import os 
-
-f = open("output.txt", "r")
-text_file = f.read()
-
-language = 'en'
-
-tts = gTTS(text=text_file, lang=language, slow=False) 
-
-# Support for multiple accents/languages to come: tld='com.country'
-
-tts.save("file.mp3") 
-
-os.system("afplay file.mp3") 
